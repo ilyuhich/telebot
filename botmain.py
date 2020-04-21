75364a306cb7c4b8abf8c67b32be3b85d0d587fc
ilyuhich <57012003+ilyuhich@users.noreply.github.com>
2020-04-21 23:41:53 +0300

    Bot can send echo message for any text

---
+++ botmain.py
@@ -0,0 +1,44 @@
+import requests
+from time import sleep
+
+url = "https://api.telegram.org/bot1213907579:AAE6CL5YvyfWlFTMzKjNloFozBl665KHt-k/"
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+def last_update (data):
+    results = data['result']
+    total_updates = len(results)-1
+    return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_sms (chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post (url + 'sendMessage', data = params)
+    return (response)
+
+#print (url)
+#print ()
+#chatid = get_chat_id(last_update(get_updates_json(url)))
+#print ('Here is Bot URL:  ' + url)
+#print ('Here is response: ' + str(get_updates_json(url)))
+#print ('Here is chat ID:  ' + str(get_chat_id(last_update(get_updates_json(url)))))
+#print (last_update(get_updates_json(url)))
+#send_mess(chatid,'sms-ka test')
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+            send_sms(get_chat_id(last_update(get_updates_json(url))), 'test')
+            update_id +=1
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
+
+print ('Program shutdown...')
